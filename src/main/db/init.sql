37544f1c477b141e5be66d537fb8fb7def35e10d
rmk1409 <rmk140987@gmail.com>
2016-09-07 12:36:39 +0300

    small fixes and add init.sql script

---
+++ src/main/db/init.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS Person;
+DROP SEQUENCE IF EXISTS seq_for_person_id;
+
+CREATE SEQUENCE seq_for_person_id;
+CREATE TABLE IF NOT EXISTS "person" (
+  "id" integer PRIMARY KEY default nextval('seq_for_person_id'),
+  "name" varchar(30) DEFAULT NULL,
+  "password" varchar(30) DEFAULT NULL,
+  "country" varchar(30) DEFAULT NULL
+);
+
+INSERT into person(name, password, country) values ('Ivan', 'PassIvan', 'Rus');
+INSERT into person(name, password, country) values ('Rmk', 'PassRmk', 'Rus');
+INSERT into person(name, password, country) values ('Alex', 'PassAlex', 'USA');
+INSERT into person(name, password, country) values ('Sala', 'PassSala', 'Europe');
